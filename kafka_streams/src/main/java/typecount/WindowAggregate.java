26eb3869a933dc5dc0ebab203a8229ccb97fe7fb
Shwetha <shwethanyn@gmail.com>
2017-06-22 12:50:52 -0400

    Updated WindowAggreagate with comments

--- kafka_streams/src/main/java/typecount/WindowAggregate.java
+++ kafka_streams/src/main/java/typecount/WindowAggregate.java
@@ -13,16 +13,28 @@ import java.util.Date;
 import java.math.BigInteger;
 import org.apache.kafka.streams.processor.*;
 
+/**
+* The WindowAggregate Processor aggregates the count of the attack types 
+* for a time window if 60 seconds and writes output to the output stream 
+* specified in driver. The State Store is used for holding the aggregated value
+*
+*/
 public class WindowAggregate extends AbstractProcessor<String, RegionSummary> {
 	private ProcessorContext context;
 	private KeyValueStore<String, String> hourlyStore;
 	
+	// Initializing Processor variables
 	public void init(ProcessorContext context) {
 		this.context = context;
 		this.context.schedule(10000);
 		this.hourlyStore = (KeyValueStore<String, String>) context.getStateStore("HourlyCount_Store");
 	}
 
+	/**
+	* This function gets called for every event in the input stream
+	* State Store is used for holding the accumulated values
+	* 
+	*/
 	public void process(String key, RegionSummary value) {
 		if( value !=null || value.attack_type != null || value.attack_type != "") {
 			String count = hourlyStore.get(value.attack_type);
@@ -38,6 +50,12 @@ public class WindowAggregate extends AbstractProcessor<String, RegionSummary> {
 			}
 		}
 	}
+	
+	/** 
+	* This function gets called at the end of the time window. It iterates through
+	* the state store and prints the results
+	*
+	*/
 
 	public void punctuate(long timestamp) {
 		KeyValueIterator<String, String> iter = this.hourlyStore.all();
