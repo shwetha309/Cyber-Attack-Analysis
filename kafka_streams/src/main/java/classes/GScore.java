5c01cf4c884d255fc577d0b096f7fe4bb0c79690
Shwetha <shwethanyn@gmail.com>
2017-06-21 21:51:36 -0400

    Removed commented portion in GScore class

--- kafka_streams/src/main/java/classes/GScore.java
+++ kafka_streams/src/main/java/classes/GScore.java
@@ -11,18 +11,5 @@ public class GScore  {
 		this.coords = coord;
 		this.gscore = g;
 	}
-        
-/*	@Override
-	public int compareTo (GScore gs1, GScore gs2)
-	{
-		double g1 = gs1.gscore;
-		double g2 = gs2.gscore;
-
-		if (g1 < g2)
-			return -1;
-		else if (g1 > g2)
-			return 1;
-		return 0;
-	}*/
 }
 		
